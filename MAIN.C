6199c6d606adcd76b616d0074e2b5c1151f5af0d
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-12-04 17:09:54 -0800

    while

--- MAIN.C
+++ MAIN.C
@@ -1,8 +1,3 @@
 #include"election.h"
 
 int main(){
-    while(1){
-        printf("\n\t\t\t   1.Student panel \n\t\t\t   2.Admin panel \n\t\t\t   3.Exit \n\t\t\t   Option:");
-		char input;
-        scanf(" %c",&input);
-
